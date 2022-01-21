fcecd947b819306a5d7d564ffa2a241da5d88f47
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-21 19:37:33 +0200

    sign up page visually ready

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,47 @@
+//react
+
+import { useState } from 'react'
 // styles
 import styles from './Signup.module.css'
 
-export default function Signup() {
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      Signup
-    </div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>login</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input 
+          type="text" 
+          onChange={(e) => setName(e.target.value)} 
+          value={name}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+        />
+      </label>
+      <button className="btn">Sign up!</button>
+    </form>
   )
 }
