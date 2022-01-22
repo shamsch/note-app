63cc3de26b8526c68e536a6ea68a6e6e63906059
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-22 02:31:09 +0200

    log out ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
-
+//hook
+import { useLogout } from "../hooks/useLogout"
 // styles
 import styles from './Navbar.module.css'
 
 export default function Navbar() {
+  const {logout}=useLogout()
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -11,6 +13,10 @@ export default function Navbar() {
         
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
+
+        <li>
+          <button className="btn" onClick={logout}>logout</button>
+        </li>
       </ul>
     </nav>
   )
