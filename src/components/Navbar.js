79e3b0d3a7c50a4e68ee940cfb1b6b9908dec51f
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-22 03:08:39 +0200

    conditional render log out and logged in user msg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,42 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 //hook
-import { useLogout } from "../hooks/useLogout"
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 // styles
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  const {logout}=useLogout()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>My Notes</li>
-        
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
 
-        <li>
-          <button className="btn" onClick={logout}>logout</button>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+          <li>
+            <p>Hello {user.displayName}</p>
+          </li>
+          <li>
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
         </li>
+          </>
+        )}
       </ul>
     </nav>
-  )
+  );
 }
